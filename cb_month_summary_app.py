364d839ee269bab94a941aa5dfb0c6d3b33b9bbf
jhever-wed <john.hever@wedbush.com>
2025-05-08 10:12:50 -0500

    Add files via upload

--- cb_month_summary_app.py
+++ cb_month_summary_app.py
@@ -40,7 +40,7 @@ if atlantis_file and gmi_file:
 
         
         # Normalize GMI column names to lowercase and strip spaces
-        df2.columns = df2.columns.str.strip().str.lower()
+        df2.columns = df2.columns.str.strip()
         df2 = df2[df2['TGIVIO'] == 'GO']
         col_map = {col: orig for col, orig in zip(df2.columns, df2.columns)}
 
